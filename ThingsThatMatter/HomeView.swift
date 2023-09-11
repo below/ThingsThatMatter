ce03c19b2f893e300384db322439d83cd8f12cb6
Alexander von Below <alex@vonbelow.com>
2023-09-11 18:36:05 +0200

    Version one: Fails to connect to connection to service named com.apple.homed.xpc

---
+++ ThingsThatMatter/HomeView.swift
@@ -0,0 +1,73 @@
+//
+//  HomeView.swift
+//  MatterCommissionerPOC
+//
+//  Created by Alexander von Below on 24.03.23.
+//
+
+import SwiftUI
+import HomeKit
+
+struct HexFormatStyle: FormatStyle {
+    var prefix: String = ""
+    func format(_ value: UInt64) -> String {
+        String(format: "\(prefix)%x", value)
+    }
+    func prefix(_ prefix: String) -> Self {
+        var result = self
+        result.prefix = prefix
+        return result
+    }
+}
+
+extension FormatStyle where Self == HexFormatStyle {
+    static var hex: HexFormatStyle {
+        HexFormatStyle()
+    }
+}
+
+@available(iOS 16.1, *)
+struct HomeView: View {
+    @StateObject private var homeManager = HomeObserver(homeManager: HMHomeManager())
+    
+    var body: some View {
+        let homes = homeManager.homes
+        List {
+            ForEach(homes, id:\.uniqueIdentifier) { home in
+                Section(home.name) {
+                    ForEach(home.accessories, id:\.uniqueIdentifier) { accessory in
+                        if let matterNodeID = accessory.matterNodeID {
+                            LabeledContent(accessory.name, value: matterNodeID, format: .hex.prefix("0x"))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+class HomeObserver: NSObject, HMHomeManagerDelegate, ObservableObject {
+    var homeManager: HMHomeManager
+    @Published var homes: [HMHome] = []
+    
+    init(homeManager: HMHomeManager) {
+        self.homeManager = homeManager
+        super.init()
+        self.homeManager.delegate = self
+    }
+    
+    func homeManagerDidUpdateHomes(_ manager: HMHomeManager) {
+        self.homes = manager.homes
+        debugPrint("Got new homes \(manager.homes)")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        if #available(iOS 16.1, *) {
+            HomeView()
+        } else {
+            Text ("iOS 16 only")
+        }
+    }
+}
