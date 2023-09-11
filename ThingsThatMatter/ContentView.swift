07335b282dce859ea54508cedb059d506f7e274e
Alexander von Below <alex@vonbelow.com>
2023-09-11 18:29:43 +0200

    Initial Commit

---
+++ ThingsThatMatter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThingsThatMatter
+//
+//  Created by Alexander von Below on 11.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThingsThatMatter/ThingsThatMatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThingsThatMatterApp.swift
+//  ThingsThatMatter
+//
+//  Created by Alexander von Below on 11.09.23.
+//
+
+import SwiftUI
+
+@main
+struct ThingsThatMatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
