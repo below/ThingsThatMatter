f94261ca0a83a251361e7859a90b5f57e66bd3ef
Alexander von Below <alex@vonbelow.com>
2023-09-22 17:19:44 +0200

    Turning on the lights

---
+++ ThingsThatMatter/DeviceView.swift
@@ -0,0 +1,18 @@
+//
+//  DeviceView.swift
+//  ThingsThatMatter
+//
+//  Created by Alexander von Below on 11.09.23.
+//
+
+import SwiftUI
+
+struct DeviceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeviceView()
+}
---
+++ ThingsThatMatter/VBBaseClusters.swift
@@ -0,0 +1,8 @@
+//
+//  VBBaseClusters.swift
+//  ThingsThatMatter
+//
+//  Created by Alexander von Below on 18.09.23.
+//
+
+import Foundation
